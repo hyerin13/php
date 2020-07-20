9dba929e2e72dcc52a638e4efaa53896948c2650
hyerin13 <61546225+hyerin13@users.noreply.github.com>
2020-07-20 18:44:35 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+     <?php
+     //php 시작
+     echo "Hello World!";
+     //php 끝 (주석 쓸 때 //대신 # 써도 됌)
+     ?> 
+</body>
+
+</html>
